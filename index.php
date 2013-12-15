42f25974089519948a2823460c3d0b1a50f82832
adriansome <adriansome@gmail.com>
2013-12-15 23:49:09 +0000

    index altered
    more changes to the index

---
+++ index.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+    
+    <head>
+         <meta chartset="utf-8" />
+        <script type="text/javascript">
+        var inputNumber = prompt('enter number', '');
+        var sqroot = Math.sqrt(inputNumber);
+        
+        
+        
+        </script>
+ 
+        
+        
+        <title>responsive javascript with PHP</title>
+   
+       <link rel="stylesheet" type="text/css" href="stylesheets/style.css">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       
+    </head>
+    <body class="body">
+        <header>
+           header 
+            
+        </header>
+        <div id="main-container">
+        <div id="left-column"> <p>
+                
+               <div id="left-column-box">
+                           <script type="text/javascript">
+        document.write('the square root of ' + inputNumber + ' = <br/>' + sqroot);
+        
+        
+        </script>
+                   
+               </div><p>the 1500s, when an unknown printer took a galley of type and scrambled it to make a 
+        type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+        remaining essentially unchanged. It was popularised 
+        in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+        and more recently with desktop publishinb</p>
+       <form name="adriansform">
+username:<input type="text" name="username">
+password:<input type="password" name="password">
+<input type="submit" value="submit">
+       </form>
+    </div>
+        
+        <div id="right-column"><p>rity have suffered alteration in some form, by injected humour, or randomised words
+            which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't
+            anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend</p></div> 
+           </div>
+        
+       <footer>
+        
+        
+        
+        footer
+    </footer>  
+    </body>
+    
+   
+    
+    
+</html>
